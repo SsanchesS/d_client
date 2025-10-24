76b9de00570331602a40ab8bcd7626af267c63bd
Alexander <100308427+SsanchesS@users.noreply.github.com>
2025-10-24 18:00:52 +0300

    Delete src directory

--- src/hooks/hooks.ts
+++
@@ -1,5 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux"
-import {TypeState,TypeDispatch} from "../store/store"
-
-export const useAppDispatch=()=>useDispatch<TypeDispatch>()
-export const useAppSelector: TypedUseSelectorHook<TypeState>=useSelector
